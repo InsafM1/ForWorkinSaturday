4e9c477fa6a11d83edca322fcf1bb7d0b19e23fc
InsafM1 <muhamadiarovinsaf0@gmail.com>
2025-11-01 13:04:50 +0300

    file.cpp

--- file.cpp
+++ file.cpp
@@ -2,9 +2,9 @@
 #include <cstddef>
 #include <limits>
 
-int ** make_mtx(int r, int c);
+int ** make(int r, int c);
 void output(const int *const* mtx);
-void rm(int ** mtx, int rows, int cols);
+void rm(int ** mtx, int r);
 
 int main() {
   int rows = 0, cols = 0;
@@ -13,15 +13,33 @@ int main() {
     return 1;
   }
   int ** mtx = nullptr;
-  mtx = make_mtx(rows, cols);
+  try {
+    mtx = make_mtx(rows, cols);
+  } catch (const std::bad_alloc &) {
+    rm(mtx, rows, cols);
+    return 2; // обработка беда - локк
+  }
+
   output(mtx);
   rm(mtx);
 }
 
-void rm(int ** mtx, int r, int c) {
+void rm(int ** mtx, int r) {
   for (size_t i = 0; i < r; i++) {
     delete[] mtx[i]; // если мы сразу удалим  int ** mtx, то к матрицам, на которые он указывает, не будем иметь доступа - будет утечка памяти
   }
-  delet[] mtx;
+  delete[] mtx;
 }
 
+int ** make(int r, int c) {
+  int ** mtx =  new int*[r]; // [*][*]...[*] r раз
+  for (size_t i = 0; i < r; i++) {
+    try {
+      mtx[i] = new int[c]; // выделаем массив
+    } catch (...) {
+      rm(mtx,i);
+      throw;
+    }
+  }
+  return mtx;
+}
